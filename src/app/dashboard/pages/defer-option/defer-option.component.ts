ebc489dbdd27c3f7d55dd749818ebe9ee15009f3
kevin cuadros <kevindavidcuadros@gmail.com>
2024-10-05 17:41:10 -0500

    create directories

---
+++ src/app/dashboard/pages/defer-option/defer-option.component.ts
@@ -0,0 +1,18 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-defer-option',
+  standalone: true,
+  imports: [
+    CommonModule,
+  ],
+  templateUrl: './defer-option.component.html',
+  styles: `
+    :host {
+      display: block;
+    }
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class DeferOptionComponent { }
