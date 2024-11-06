1761a44875c065899b565b46d6630fe466b00474
kevin cuadros <kevindavidcuadros@gmail.com>
2024-11-06 16:13:31 -0500

    Change Detection

--- src/app/dashboard/pages/change-detection/change-detection.component.ts
+++ src/app/dashboard/pages/change-detection/change-detection.component.ts
@@ -1,18 +1,48 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, signal } from '@angular/core';
+import { TitleComponent } from '@shared/title/title.component';
 
 @Component({
   selector: 'app-change-detection',
   standalone: true,
-  imports: [
-    CommonModule,
-  ],
+  imports: [CommonModule, TitleComponent],
   templateUrl: './change-detection.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   styles: `
     :host {
       display: block;
     }
   `,
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class ChangeDetectionComponent { }
+export default class ChangeDetectionComponent {
+
+  public currentFramework = computed(
+    () => `Change Detection: ${this.frameworkAsSignal().name}`
+  )
+
+  public frameworkAsSignal = signal({
+    name: 'Angular',
+    releaseDate: 2016
+  })
+
+  public frameworkAsProperty = {
+    name: 'Angular',
+    releaseDate: 2016
+  }
+
+  constructor() {
+    setTimeout(() => {
+      
+      // this.frameworkAsProperty.name = 'React';
+
+      this.frameworkAsSignal.update((state) => ({
+        ...state,
+        name: 'React'
+      }))
+
+      console.log("done");
+    }, 3000);
+  }
+  
+
+}
